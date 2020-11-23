1628c2056b41784d70e1d590406704de0900d33f
Martin Moradi <moradi.martin@gmail.com>
2020-11-23 11:03:50 +0100

    Devise users, active storage, user validations

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  has_one_attached :avatar
+  validates_presence_of :firstname, :lastname
+  validates :email, uniqueness: true
+end
