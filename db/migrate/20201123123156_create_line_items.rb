6e2271c89499d97ddf9df6f91d08783427c47924
Martin Moradi <moradi.martin@gmail.com>
2020-11-23 13:33:14 +0100

    LineItems model & table + validations

---
+++ db/migrate/20201123123156_create_line_items.rb
@@ -0,0 +1,11 @@
+class CreateLineItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :line_items do |t|
+      t.references :cart, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity, default: 1
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_122437) do
+ActiveRecord::Schema.define(version: 2020_11_23_123156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2020_11_23_122437) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", default: 1
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.bigint "cart_id", null: false
     t.bigint "user_id", null: false
@@ -96,6 +106,8 @@ ActiveRecord::Schema.define(version: 2020_11_23_122437) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "users"
   add_foreign_key "carts", "users"
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
   add_foreign_key "orders", "addresses"
   add_foreign_key "orders", "carts"
   add_foreign_key "orders", "users"
