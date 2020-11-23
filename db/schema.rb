c4b817f922a6b30d3300cdb2dd325b005a41be9e
Martin Moradi <moradi.martin@gmail.com>
2020-11-23 12:34:38 +0100

    Cart Model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_110307) do
+ActiveRecord::Schema.define(version: 2020_11_23_113250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2020_11_23_110307) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -77,4 +84,5 @@ ActiveRecord::Schema.define(version: 2020_11_23_110307) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "users"
+  add_foreign_key "carts", "users"
 end
