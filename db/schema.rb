bc3d33c5e45f69b39ee3a0b7b7c5643134b8bf75
Martin Moradi <moradi.martin@gmail.com>
2020-11-23 13:42:58 +0100

    join table Products & Category into development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_123632) do
+ActiveRecord::Schema.define(version: 2020_11_23_124149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2020_11_23_123632) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "assigment_product_categories", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_assigment_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_assigment_product_categories_on_product_id"
   end
 
   create_table "carts", force: :cascade do |t|
@@ -123,6 +132,8 @@ ActiveRecord::Schema.define(version: 2020_11_23_123632) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "users"
+  add_foreign_key "assigment_product_categories", "categories"
+  add_foreign_key "assigment_product_categories", "products"
   add_foreign_key "carts", "users"
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "products"
---
+++ test/models/assigment_product_category_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AssigmentProductCategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
