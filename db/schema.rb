a893a537d46e066fdd74643428b22995e3a080f8
Martin Moradi <moradi.martin@gmail.com>
2020-11-23 15:49:37 +0100

    Added Column Img_Url to Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_124149) do
+ActiveRecord::Schema.define(version: 2020_11_23_144608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_124149) do
     t.decimal "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img_url", null: false
   end
 
   create_table "ratings", force: :cascade do |t|
