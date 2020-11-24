8aad68278a2e1038b6bf81891cc986cb7bc73618
Martin Moradi <moradi.martin@gmail.com>
2020-11-24 20:37:33 +0100

    Disabled Devise Email conformations for seeding Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         
+         :recoverable, :rememberable, :validatable
 
-         # :confirmable -> disabled devise email confirmation for heroku seeding purposes
+  # :confirmable -> disabled devise email confirmation for heroku seeding purposes
 
   validates_presence_of :firstname, :lastname
   validates :email, uniqueness: true
