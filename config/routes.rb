9269b92891d03a20a53f1f017de3b6d9681fa8fd
Martin Moradi <moradi.martin@gmail.com>
2020-11-24 22:56:44 +0100

    updated devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,12 @@ Rails.application.routes.draw do
   resources :products
   resources :orders
 
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: "users/registrations" }, path: "/", path_names: { sign_in: "log_in",
+                                                                                                     sign_out: "logout",
+                                                                                                     password: "secret",
+                                                                                                     confirmation: "verification",
+                                                                                                     unlock: "unblock",
+                                                                                                     sign_up: "sign_up" }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
