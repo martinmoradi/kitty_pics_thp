4df7d442e7e4afcef82caa114cfce16397666e38
Uriel <uriel.sautron@gmail.com>
2020-11-25 15:27:18 +0100

    Add mailjet

--- Gemfile
+++ Gemfile
@@ -81,6 +81,11 @@ gem "activerecord-reset-pk-sequence"
 
 # Random data generator
 gem "faker", :git => "https://github.com/faker-ruby/faker.git", :branch => "master"
+
+# Mailjet
+gem 'mailjet', :git => 'https://github.com/mailjet/mailjet-gem.git'
+
+
 #################################################################################
 
 # List of added gems :
@@ -96,5 +101,5 @@ gem "faker", :git => "https://github.com/faker-ruby/faker.git", :branch => "mast
 # gem "stripe"
 # gem "activerecord-reset-pk-sequence"
 # gem "table_print"
-
+# gem 'mailjet', :git => 'https://github.com/mailjet/mailjet-gem.git'
 #################################################################################
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
   root "products#index"
 
-<<<<<<< HEAD
   
   resources :carts
   resources :products do 
     resources :line_items
   end
   
-=======
-  get "carts/:id" => "carts#show", as: "my_cart"
-  delete "carts/:id" => "carts#destroy"
-
-  post "line_items/:id/add" => "line_items#add_quantity", as: "line_item_add"
-  post "line_items/:id/reduce" => "line_items#reduce_quantity", as: "line_item_reduce"
-  post "line_items" => "line_items#create"
-  get "line_items/:id" => "line_items#show", as: "line_item"
-  delete "line_items/:id" => "line_items#destroy"
-
-  resources :products
->>>>>>> d09e2edb4277dfc3f9bcc7bf697348377047dd8e
   resources :orders
 
   devise_for :users, controllers: { registrations: "users/registrations" }, path: "/", path_names: { sign_in: "log_in",
