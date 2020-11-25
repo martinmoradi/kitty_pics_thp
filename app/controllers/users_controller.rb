f78cffcd8dda4b122323017bacccc9d30c4a1be6
Joe-Pumpkin <jonathan.lalanne@outlook.fr>
2020-11-25 22:57:57 +0100

    Modif user show edit / clean cards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,12 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+
+  def update
+    @user =  current_user
+    user_params =  params.require(:user).permit(:firstname, :lastname, :email, :adress)
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
 end
