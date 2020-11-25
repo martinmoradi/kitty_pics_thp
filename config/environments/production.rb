1807b0a3d8952bb096a58e580579f7ffbfbd3a9d
Martin Moradi <moradi.martin@gmail.com>
2020-11-25 23:08:40 +0100

    mailtesting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "thp_kittens_shop_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
   config.action_mailer.default_url_options = { host: "kitty-pics.herokuapp.com" }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
