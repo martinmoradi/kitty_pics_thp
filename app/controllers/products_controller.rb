3b5ae28a78101f9225d7532ca1e91c92c52dcf05
Joe-Pumpkin <jonathan.lalanne@outlook.fr>
2020-11-25 11:41:13 +0100

    Adding edit_views modif index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,9 +28,13 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:name, :price)
+    params.require(:product).permit(:title, :price, :img_url, :description)
   end
 end
