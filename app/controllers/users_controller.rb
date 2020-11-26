4b73e629e8e069b39f2ff496f7bf7da40c20eaf7
Martin Moradi <moradi.martin@gmail.com>
2020-11-26 10:32:34 +0100

    merging front views products/users updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = current_user
+    user_params = params.require(:user).permit(:firstname, :lastname, :email, :adress)
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
 end
